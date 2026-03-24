b50c708b94787efe92cbafbba37f3196623d9fdd
Vlad Rindevich <github.aqwb5@mail.ausginer.com>
2026-03-24 17:25:26 +0000

    chore: use prebuilt devimage

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -e
 
-sudo chown -R node:node /workspaces/material-x
-
-# Install npm dependencies
-cd /workspaces/material-x
-npm install
-
-# Install Playwright browsers and system dependencies
-npx playwright install chromium
-npx playwright install-deps chromium
+sudo chown -R node:node /workspaces/material-x/node_modules
