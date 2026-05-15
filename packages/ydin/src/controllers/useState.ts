f0cdd925fcbc304b7f5b97bd28575c3e318dccd8
Vlad Rindevich <github.aqwb5@mail.ausginer.com>
2026-05-15 19:17:03 +0000

    feat(ydin): make useState more powerful

--- packages/ydin/src/controllers/useState.ts
+++ packages/ydin/src/controllers/useState.ts
@@ -1,17 +1,21 @@
 import { Bool } from '../attribute.ts';
 import { getInternals, type ControlledElement } from '../element.ts';
 import { toggleState } from '../utils/DOM.ts';
-import { useAttributes, via } from './useAttributes.ts';
+import { useAttributes } from './useAttributes.ts';
+
+export type StateCondition = (value: string | null) => boolean;
 
 export function useState<T extends ControlledElement>(
   host: T,
   attribute: keyof T & string,
+  state: string = attribute,
+  condition: StateCondition = Bool.from,
 ): void {
   const internals = getInternals(host);
 
   useAttributes(host, {
-    [attribute]: via(Bool, (_, value) =>
-      toggleState(internals, attribute, value),
-    ),
+    [attribute](_, value) {
+      toggleState(internals, state, condition(value));
+    },
   });
 }
